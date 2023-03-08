1784dcc8fc5401bceb0f3a5a8498f1dd4683ad5b
Devilsking <leonguyen52@outlook.com>
2023-03-09 00:34:38 +0700

    Update config.py

--- config.py
+++ config.py
@@ -3,14 +3,14 @@
 # Bot config
 BotAPIKey = 'xxx' # API Keythat you get from @BotFather
 tg = xxx # Your tg id, you can get it by sending command /id to bot @get_my_chat_id_bot
-serverbotpath = '/root/serverbot' # User folder with this bot.
-serverbotpathdb = '/root/serverbot/db' # User folder with bot database. 
+serverbotpath = '/root/serverbot-tg' # User folder with this bot.
+serverbotpathdb = '/root/serverbot-tg/db' # User folder with bot database. 
 srvping = '1.1.1.1' # Ping test server
 traceroutetest = '1.1.1.1' # Traceroute test server
 
 # Server config 
 memloadalarm = 90 # RAM Utilization alarm starts at
-pingcalarm = 200 # When ping will be more than X ms, you will get alarm.
+pingcalarm = 350 # When ping will be more than X ms, you will get alarm.
 cpuutilalarm = 99 # CPU Utilization alarm starts at
 repeattimealarmsrv = [5,15,25,30,60,90,120,180,320, 640, 1280, 2560, 5120, 10240, 20480, 40960, 81920] # Notify every x second about high CPU, RAM load and ping
 cfgAlertsNotificationsRam = 1 # RAM Monitoring + history
